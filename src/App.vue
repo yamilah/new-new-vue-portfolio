7dbb021a9dd2477081402f8845f60103378367c9
Yamilah Atallah <yami@yamilah.com>
2018-04-01 16:05:53 -0700

    Fetch the Bootstrap 4 grid from the cloudflare CDN

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
 
 <script>
 import '../node_modules/normalize.css/normalize.css'
-import '../node_modules/bootstrap/dist/css/bootstrap-grid.css'
 import '../node_modules/simple-line-icons/fonts/**'
 import '../node_modules/simple-line-icons/css/simple-line-icons.css'
 
